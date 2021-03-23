bc9b00dbcc549bd05ae4db7bb4b5b877a2a67fa9
Samuel Dubovec <samodubovec@gmail.com>
2021-03-23 18:59:59 +0100

    Initial commit

---
+++ src/main/java/sk/zvjs/holup/HolupApplication.java
@@ -0,0 +1,13 @@
+package sk.zvjs.holup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sk/zvjs/holup/HolupApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.zvjs.holup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
